e3ae32bc1dd895959375dd0741eef26c7bdc470c
justn-gpt <146205844+justn-gpt@users.noreply.github.com>
2025-02-02 16:07:23 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -1,3 +1,6 @@
+devil www del $(whoami).serv00.net
+devil www del keep.$(whoami).serv00.net
+rm -rf $HOME/$(whoami)/domains/*
 ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
 chmod -R 755 ~/* \
 chmod -R 755 ~/.* \
